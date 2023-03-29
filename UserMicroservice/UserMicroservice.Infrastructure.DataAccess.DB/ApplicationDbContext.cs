2001d2e310ae7c3e83690268cf71eab3a641e5cd
slonikappa <maxim.kirnos1@gmaiil.com>
2023-03-29 21:11:58 +0300

    Make default project structure

---
+++ UserMicroservice/UserMicroservice.Infrastructure.DataAccess.DB/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserMicroservice.Infrastructure.DataAccess.DB;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext() : base()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
