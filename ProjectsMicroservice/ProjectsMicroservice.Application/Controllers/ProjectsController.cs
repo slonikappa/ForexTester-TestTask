90588fecbdb6f7f17c69aaea376f8be732ae4282
slonikappa <maxim.kirnos1@gmaiil.com>
2023-04-01 23:32:23 +0300

    Project preparation: Add Application layer with empty controller, remove default files. use Swagger for Release

---
+++ ProjectsMicroservice/ProjectsMicroservice.Application/Controllers/ProjectsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectsMicroservice.Application.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProjectsController : ControllerBase
+{
+    
+}
