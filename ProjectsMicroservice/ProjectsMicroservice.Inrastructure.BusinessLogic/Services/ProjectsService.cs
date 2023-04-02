088810d52659a704dd9609e2971d1950b71363ce
slonikappa <maxim.kirnos1@gmaiil.com>
2023-04-03 02:04:40 +0300

    Add services

---
+++ ProjectsMicroservice/ProjectsMicroservice.Inrastructure.BusinessLogic/Services/ProjectsService.cs
@@ -0,0 +1,53 @@
+﻿using ProjectMicroservices.Infrastructure.External.UsersMicroservice;
+using ProjectMicroservices.Infrastructure.External.UsersMicroservice.Enums;
+
+using ProjectsMicroservice.Domain.Core.Entities;
+using ProjectsMicroservice.Domain.Infrastructure.Interfaces;
+using ProjectsMicroservice.Domain.Infrastructure.Models;
+
+namespace ProjectsMicroservice.Inrastructure.BusinessLogic.Services;
+
+public class ProjectsService : IProjectsService
+{
+    private readonly IProjectsRepository _projectsRepository;
+    private readonly IUsersMicroservice _usersMicroserviceClient;
+
+    public ProjectsService(IProjectsRepository projectsRepository,
+        IUsersMicroservice usersMicroserviceClient)
+    {
+        _projectsRepository = projectsRepository;
+        _usersMicroserviceClient = usersMicroserviceClient;
+    }
+
+    public async Task AddProject(Project project)
+    {
+       await _projectsRepository.Create(project);
+    }
+
+    public async Task DeleteProject(int userId)
+    {
+       await _projectsRepository.Delete(userId);
+    }
+
+    public async Task<Project> GetProjectByUserId(int usedId)
+    {
+        return await _projectsRepository.GetById(usedId);
+    }
+
+    public async Task<IEnumerable<Project>> GetProjectList()
+    {
+        return await _projectsRepository.GetAll();
+    }
+
+    public async Task UpdateProject(Project project)
+    {
+        await _projectsRepository.Update(project);
+    }
+
+    public async Task<List<IndicatorCount>> GetMostUsedProjectIndicatorsBySubsType(SubscriptionType type)
+    {
+        var superUsers = await _usersMicroserviceClient.GetAllBySubscriptionType(type);
+
+        return _projectsRepository.GetIndicatorsByMultipleUserId(superUsers.Select(x => x.Id).ToArray());
+    }
+}
---
+++ ProjectsMicroservice/ProjectsMicroservice.Inrastructure.BusinessLogic/Services/UserSettingsService.cs
@@ -0,0 +1,45 @@
+﻿using ProjectsMicroservice.Domain.Core.Entities;
+using ProjectsMicroservice.Domain.Infrastructure.Interfaces;
+
+namespace ProjectsMicroservice.Inrastructure.BusinessLogic.Services;
+
+public class UserSettingsService : IUserSettingsService
+{
+    private readonly IRepository<UserSettings> _userSettingsRepository;
+
+    public UserSettingsService(IRepository<UserSettings> userSettingsRepository)
+    {
+        _userSettingsRepository = userSettingsRepository;
+    }
+
+    public async Task AddSettings(UserSettings userSettings)
+    {
+        await _userSettingsRepository.Create(userSettings);
+    }
+
+    public async Task DeleteSettings(int userId)
+    {
+        await _userSettingsRepository.Delete(userId);
+    }
+
+    public async Task<UserSettings> GetSettingsByUserId(int usedId)
+    {
+        return await _userSettingsRepository.GetById(usedId);
+    }
+
+    public async Task<IEnumerable<UserSettings>> GetSettingsList()
+    {
+        return await _userSettingsRepository.GetAll();
+    }
+
+    public async Task<IEnumerable<UserSettings>> GetAll()
+    {
+        return await _userSettingsRepository.GetAll();
+    }
+
+    public async Task UpdateSettings(UserSettings userSettings)
+    {
+        await _userSettingsRepository.Update(userSettings);
+    }
+}
+ 
