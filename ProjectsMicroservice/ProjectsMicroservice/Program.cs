d0128b8ebc57d7819dbf1f954b1c8891aff5964e
slonikappa <maxim.kirnos1@gmaiil.com>
2023-04-03 02:05:37 +0300

    Add dependencies, replace Dockerfile, edit appsettings

--- ProjectsMicroservice/ProjectsMicroservice/Program.cs
+++ ProjectsMicroservice/ProjectsMicroservice/Program.cs
@@ -1,8 +1,45 @@
+using System.Text.Json.Serialization;
+
+using MongoDB.Driver;
+
+using ProjectMicroservices.Infrastructure.External.UsersMicroservice;
+
+using ProjectsMicroservice.Domain.Core.Entities;
+using ProjectsMicroservice.Domain.Core.Options;
+using ProjectsMicroservice.Domain.Infrastructure.Interfaces;
+using ProjectsMicroservice.Infrastructure.DataAccess.Repositories;
+using ProjectsMicroservice.Inrastructure.BusinessLogic.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddScoped<IRepository<UserSettings>, UserSettingsRepository>();
+builder.Services.AddScoped<IProjectsRepository, ProjectsRepository>();
+
+builder.Services.AddScoped<IUserSettingsService, UserSettingsService>();
+builder.Services.AddScoped<IProjectsService, ProjectsService>();
+
+builder.Services.AddScoped<IUsersMicroservice, UsersMicroserviceClient>();
+
+// Register options
+var mongoDbOptionsSection = builder.Configuration.GetSection("Application:MongoDB");
+builder.Services.Configure<MongoDbOptions>(mongoDbOptionsSection);
+
+// Database registration
+var mongoDbOptions = mongoDbOptionsSection.Get<MongoDbOptions>();
+
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoDbOptions.ConnectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
