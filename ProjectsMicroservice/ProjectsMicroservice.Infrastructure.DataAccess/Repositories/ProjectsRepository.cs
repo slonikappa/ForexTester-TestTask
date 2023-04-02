66cd1a0893580f8fc155000f5fdc36ae2a49de5b
slonikappa <maxim.kirnos1@gmaiil.com>
2023-04-03 02:04:09 +0300

    Add repositories

---
+++ ProjectsMicroservice/ProjectsMicroservice.Infrastructure.DataAccess/Repositories/ProjectsRepository.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Extensions.Options;
+
+using MongoDB.Driver;
+
+using ProjectsMicroservice.Domain.Core.Entities;
+using ProjectsMicroservice.Domain.Core.Options;
+using ProjectsMicroservice.Domain.Infrastructure.Interfaces;
+using ProjectsMicroservice.Domain.Infrastructure.Models;
+
+namespace ProjectsMicroservice.Infrastructure.DataAccess.Repositories;
+
+public class ProjectsRepository : IProjectsRepository
+{
+    private readonly IMongoCollection<Project> _projects;
+
+    public ProjectsRepository(IMongoClient mongoClient, IOptions<MongoDbOptions> mongoDbOptions)
+    {
+        if (string.IsNullOrWhiteSpace(mongoDbOptions.Value.DatabaseName))
+        {
+            throw new ArgumentException("Database name cannot be null or whitespace.", nameof(mongoDbOptions));
+        }
+
+        var database = mongoClient.GetDatabase(mongoDbOptions.Value.DatabaseName);
+        _projects = database.GetCollection<Project>(mongoDbOptions.Value.ProjectsCollection);
+    }
+
+    public async Task<IEnumerable<Project>> GetAll(int userId = 0, int skip = 0, int limit = 0)
+    {
+        FilterDefinition<Project>? filter = null;
+
+        if (userId > 0)
+        {
+            filter = Builders<Project>.Filter.Eq(p => p.UserId, userId);
+        }
+
+        var options = new FindOptions<Project>();
+        if (skip > 0)
+        {
+            options.Skip = skip;
+        }
+        if (limit > 0)
+        {
+            options.Limit = limit;
+        }
+
+        var entities = filter is not null
+            ? await _projects.FindAsync(filter, options)
+            : await _projects.FindAsync(_ => true, options);
+
+        return await entities.ToListAsync();
+    }
+
+    public async Task<Project> GetById(int userId)
+    {
+        var filter = Builders<Project>.Filter.And(
+            Builders<Project>.Filter.Eq(p => p.UserId, userId)
+        );
+
+        return await _projects.Find(filter).FirstOrDefaultAsync();
+    }
+
+    public async Task Create(Project project)
+    {
+        if (project is null)
+        {
+            throw new ArgumentNullException(nameof(project), "Create - Project cannot be null");
+        }
+
+        await _projects.InsertOneAsync(project);
+    }
+
+    public async Task<bool> Update(Project project)
+    {
+        if (project is null)
+        {
+            throw new ArgumentNullException(nameof(project), "Update - Project cannot be null");
+        }
+
+        var result = await _projects.ReplaceOneAsync(p => p.UserId == project.UserId, project);
+        return result.IsAcknowledged && result.ModifiedCount > 0;
+    }
+
+    public async Task<bool> Delete(int userId)
+    {
+        var result = await _projects.DeleteOneAsync(p => p.UserId == userId);
+        return result.IsAcknowledged && result.DeletedCount > 0;
+    }
+
+    public List<IndicatorCount> GetIndicatorsByMultipleUserId(int[] userIds)
+    {
+        var mostUsedIndicators = _projects.AsQueryable()
+            .Where(project => userIds.Contains(project.UserId))
+            .SelectMany(document => document.Charts)
+            .SelectMany(chart => chart.Indicators)
+            .GroupBy(indicator => indicator.Name)
+            .Select(g => new IndicatorCount
+            {
+                Name = g.Key,
+                Used = g.Count()
+            })
+            .OrderByDescending(result => result.Used)
+            .ThenBy(result => result.Name)
+            .Take(3)
+            .ToList();
+
+        return mostUsedIndicators;
+    }
+}
---
+++ ProjectsMicroservice/ProjectsMicroservice.Infrastructure.DataAccess/Repositories/UserSettingsRepository.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Extensions.Options;
+
+using MongoDB.Driver;
+
+using ProjectsMicroservice.Domain.Core.Options;
+using ProjectsMicroservice.Domain.Core.Entities;
+using ProjectsMicroservice.Domain.Infrastructure.Interfaces;
+using MongoDB.Bson;
+
+namespace ProjectsMicroservice.Infrastructure.DataAccess.Repositories;
+
+public class UserSettingsRepository : IRepository<UserSettings>
+{
+    private readonly IMongoCollection<UserSettings> _userSettings;
+
+    public UserSettingsRepository(IMongoClient mongoClient, IOptions<MongoDbOptions> mongoDbOptions)
+    {
+        if (string.IsNullOrWhiteSpace(mongoDbOptions.Value.DatabaseName))
+        {
+            throw new ArgumentException("Database name cannot be null or whitespace.", nameof(mongoDbOptions));
+        }
+
+        var database = mongoClient.GetDatabase(mongoDbOptions.Value.DatabaseName);
+        _userSettings = database.GetCollection<UserSettings>(mongoDbOptions.Value.UserSettingsCollection);
+    }
+
+    public async Task<IEnumerable<UserSettings>> GetAll(int userId = 0, int skip = 0, int limit = 0)
+    {
+        FilterDefinition<UserSettings>? filter = null;
+
+        if (userId > 0)
+        {
+            filter = Builders<UserSettings>.Filter.Eq(p => p.UserId, userId);
+        }
+
+        var options = new FindOptions<UserSettings>();
+        if (skip > 0)
+        {
+            options.Skip = skip;
+        }
+        if (limit > 0)
+        {
+            options.Limit = limit;
+        }
+
+        var entities = filter is not null
+            ? await _userSettings.FindAsync(filter, options)
+            : await _userSettings.FindAsync(_ => true, options);
+
+        return await entities.ToListAsync();
+    }
+
+    public async Task<UserSettings> GetById(int userId)
+    {
+        return await _userSettings.Find(u => u.UserId == userId).FirstOrDefaultAsync();
+    }
+
+    public async Task Create(UserSettings userSettings)
+    {
+        if (userSettings is null)
+        {
+            throw new ArgumentNullException(nameof(userSettings), "Create - User settings cannot be null");
+        }
+
+        await _userSettings.InsertOneAsync(userSettings);
+    }
+
+    public async Task<bool> Update(UserSettings userSettings)
+    {
+        if (userSettings is null)
+        {
+            throw new ArgumentNullException(nameof(userSettings), "Update - User settings cannot be null");
+        }
+
+        var result = await _userSettings.ReplaceOneAsync(u => u.UserId == userSettings.UserId, userSettings);
+        return result.IsAcknowledged && result.ModifiedCount > 0;
+    }
+
+    public async Task<bool> Delete(int userId)
+    {
+        var result = await _userSettings.DeleteOneAsync(u => u.UserId == userId);
+        return result.IsAcknowledged && result.DeletedCount > 0;
+    }
+}
